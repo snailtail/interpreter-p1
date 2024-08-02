c89e05f9be8812fd084992f2ab1b320095f0168f
Magnus Olofsson <magnusolofsson@outlook.com>
2024-08-02 09:20:37 +0200

    Update hello-world

--- hello-world.java
+++ hello-world.java
@@ -2,6 +2,6 @@ class helloworld
 { 
     public static void main(String []args) 
     { 
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World! From inside Dev Container (?)"); 
     } 
 }
